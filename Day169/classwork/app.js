6d7b311507b8abffae745f4793c1da1041dcb77b
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2025-08-25 18:19:05 +0400

    commit

--- Day169/classwork/app.js
+++ Day169/classwork/app.js
@@ -9,9 +9,11 @@ const tours = JSON.parse(fs.readFileSync("./tours.json"))
 // შექმენით route რომლის მეთოდიც იქნება GET და ბილიკი /tours, გაითვალისწინეთ ის ფაქტი რომ ეს როუტე აბრუნებს მთლიან ტურების მასივს, მაგრამ შესაძლოა მიიღოთ  query სახელად sort რომელშიც გადმოგეცემათ ან price ან -price, თუ გადმოგეცათ price მაგ შემთვევაში დაალაგეთ ზრდადობით ტურების მასივი და დაუბრუნეთ დალაგებული მასივი (შენიშნვნა არ შეცვლაოტ ორიგინალი მასივი) მაგრამ თუ გადმოგეცათ -price მაგ შემთხვევაში დაალაგეთ ტურების მასივი კლ;ებადობით ფასის მიხედვით, თუ საერთოდ არ გადმოგეცათ sort query მაგ შემთხვევაში დააბრუნეთ ჩვეულებრივად ტურების მასივი 
 
 app.get("/tours", (req, res) => {
-  const { sort } = req.query;
+  const { sort,limit } = req.query;
   const result = [...tours];
-
+  if(limit){
+    result.splice(limit,tours.length - limit)
+  }
   if (sort === "price") {
     result.sort((a, b) => Number(a.price) - Number(b.price));
   } else if (sort === "-price") {
