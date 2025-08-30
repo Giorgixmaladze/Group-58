13be4130406661a6c44ee57173c557114e8ed8b5
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2025-08-30 20:16:55 +0400

    commit

---
+++ Day170/classwork/task1&2/app.js
@@ -0,0 +1,39 @@
+const express = require("express")
+
+const fs = require("fs")
+
+const app = express()
+
+const tours= JSON.parse(fs.readFileSync("../tours.json"))
+
+
+
+app.get("/tours", (req,res) =>{
+    const {sort} = req.query
+
+    const copiedTours = [...tours]
+
+    if(sort==="duration"){
+        copiedTours.sort((a,b) =>a.duration - b.duration)
+    }else if(sort === "-duration"){
+        copiedTours.sort((a,b) => b.duration - a.duration)
+    }
+    res.json(copiedTours)
+
+})
+
+
+app.get("/tours/:id",(req,res) =>{
+    const {id} = req.params
+
+    const tour  =  tours.find((el) => el.id === id)
+    
+    if(!tour){
+        return res.status(404).json({message:"Tour not found"})
+    }
+    res.json(tour)
+})
+
+app.listen(3000,() => {
+    console.log("Server is running")
+})
